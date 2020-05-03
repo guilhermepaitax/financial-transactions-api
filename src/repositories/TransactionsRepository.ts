f9b5be9966a0c5b68509ad20e7e980a0111ec44a
Guilherme Paitax <guilhermepaitax16@gmail.com>
2020-05-02 21:30:31 -0300

    Adicionando funcionalidades

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,23 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    let income = 0;
+    let outcome = 0;
+
+    transactions.forEach(transaction => {
+      if (transaction.type === 'income') income += Number(transaction.value);
+      else outcome += Number(transaction.value);
+    });
+
+    const balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return balance;
   }
 }
 
